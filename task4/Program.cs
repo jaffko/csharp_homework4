1f0b84aace2c514c3c0484163bd53c622d63decf
Ildar <steel14@yandex.ru>
2022-09-18 00:59:08 +0500

    added task4

---
+++ task4/Program.cs
@@ -0,0 +1,21 @@
+﻿// Напишите программу, которая принимает на вход число (целое или вещественное) и выдаёт количество цифр в числе.
+// 456 -> 3
+// 0.78 -> 2
+// 89.126 -> 5
+
+int digitsNum(double N)
+{
+    while (N % 1 !=0) N = N * 10;
+    int numLen = 1;
+    int i = 10;
+    while (N % i != N)
+    {
+        numLen++;
+        i = i * 10;
+    }
+    return numLen;
+}
+
+Console.WriteLine("Введите число:");
+double num = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine($"Количество цифр в числе {num} равно {digitsNum(num)}");
