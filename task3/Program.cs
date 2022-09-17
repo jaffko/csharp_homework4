2dd7aff35a2822431962cabc8f6375d2276ec7b0
Ildar <steel14@yandex.ru>
2022-09-18 00:04:26 +0500

    added task3

---
+++ task3/Program.cs
@@ -0,0 +1,16 @@
+﻿//  Напишите программу, которая задаёт массив из N элементов и выводит их на экран. N - вводится с клавиатуры. Элементы тоже вводятся с клавиатуры.
+// 5
+// 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+// 3
+// 6, 1, 33 -> [6, 1, 33]
+
+int[] newArray(int N)
+{
+    int[] result = new int[N];
+    for (int i = 0; i < N; i++) result[i] = Convert.ToInt32(Console.ReadLine());
+    return result;
+}
+
+Console.WriteLine("Введите размер массива N");
+int N = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(string.Join(", ", newArray(N)));
